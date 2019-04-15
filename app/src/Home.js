a6e04689e211c4b528db9eaa5b300b9e62096eb8
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-04-15 17:08:29 -0400

    Added a jumbotron

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import {Button, Jumbotron, NavItem, NavLink} from "reactstrap";
+import {Link} from "react-router-dom";
 
 /*
 This page introduces HorsEther to the user
@@ -6,7 +8,17 @@ This page introduces HorsEther to the user
 class Home extends Component {
     render() {
         return (
-            <p>This homepage explains what horsether is</p>
+            <div className="container">
+                <Jumbotron>
+                    <h1 className="display-3">Welcome to HorsEther</h1>
+                    <p className="lead">Place bets on simulated horse races with Ethereum</p>
+                    <hr className="my-2" />
+                    <p>Here's another line for some more info</p>
+                    <p className="lead">
+                        <Button color="warning"><Link  to='/bet' style={{ textDecoration: 'none', color: 'black' }}>Get Started</Link></Button>
+                    </p>
+                </Jumbotron>
+            </div>
         );
     }
 }
