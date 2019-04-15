45a44524459609e7e03fbcdbe465e3bfa2a0fce5
Aarohi Savaliya <savaliya_aarohi@yahoo.in>
2019-04-15 15:48:11 -0400

    fixed errors

--- contracts/HorsEther.sol
+++ contracts/HorsEther.sol
@@ -4,15 +4,13 @@ contract HorsEther {
 
     struct Horse{
         uint horseNumber;
-        byte32 horseName;
+        string horseName;
     }
 
     struct Race {
         uint raceId;
         Horse[] horses;
         Bet[] bets;
-        uint numOfBets;
-        mapping(address => Bet) betsInfo;
     }
 
     struct Bet {
@@ -24,37 +22,35 @@ contract HorsEther {
     //get list of races, check evaluated? expires
     //valid races
     uint numOfRaces;
+    string[] horseNames = ["horse1","horse2","horse3","horse4","horse5"];
 
-    mapping(uint => Horse) public horsesInfo; //bet amount on horse
+    //bet amount on horse
     mapping(uint => Race) public races; //race array
 
-
     function createRace() public {
-        Horse[] storage _horses;
-        _horses.push(Horse(1,"Horse1"));
-        Bet[] storage _bets;
+        uint raceId;
         raceId= numOfRaces++;
-        races[raceId] = Race(_bets, _horses);
+        for(uint i=1 ; i<6; i++){
+            races[raceId].horses.push(Horse(i, horseNames[i-1]));
+        }
     }
 
     //check if the bettor exist in the race already
     //get the race based in raceId
     //add bettor to the race and add horsesInfo to the race
     //add amount
-    function createBet(uint _horseNumber, uint raceId, uint amount) public payable{
-        require(!checkBettorExists(msg.sender));
-        require(msg.value >= amount);
-        Race storage r= races[raceId];
-        r.bets[r.numOfBets++]= Bet(msg.sender, false, _horseNumber, amount);
-        horsesInfo[_horseNumber] += msg.value;
-        r.horses.push(horsesInfo);
+    function createBet(uint _horseNumber, uint _raceId, uint _amount) public payable{
+        require(!checkBettorExists(msg.sender, _raceId));
+        require(msg.value >= _amount);
+        Race storage r= races[_raceId];
+        r.bets.push(Bet(msg.sender, false, _horseNumber, _amount));
     }
 
     //check if the bettor has already participated in betting for specific race
     function checkBettorExists(address bettor, uint raceId) public view returns(bool){
         Race storage r= races[raceId];
-        for(uint i = 0; i < r.bettorsInfo.length; i++){
-            if(r.bettorsInfo[i].addr == bettor) return true;
+        for(uint i = 0; i < r.bets.length; i++){
+            if(r.bets[i].bettorAddr == bettor) return true;
         }
         return false;
     }
