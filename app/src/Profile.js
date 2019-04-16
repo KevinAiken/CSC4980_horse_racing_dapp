16bbe11dc980e78cfec6ddd8b6661ec0a80a5e34
Kevin Aiken <Aiken.Kevin.M@gmail.com>
2019-04-16 16:52:05 -0400

    implemented balance and address in profile

--- app/src/Profile.js
+++ app/src/Profile.js
@@ -4,13 +4,19 @@ import React, {Component} from "react";
 This page shows the user their ethereum address and balance
  */
 class Profile extends Component {
-    state = { dateKey: null };
+    state = { dateKey: null, accountAddress: null, accountBalance: null };
 
     componentDidMount() {
         const {drizzle} = this.props;
         const contract = drizzle.contracts.MyStringStore;
 
         const dataKey = contract.methods["myString"].cacheCall();
+
+
+        drizzle.web3.eth.getAccounts().then(e => this.setState({ accountAddress: e[0]}));
+
+        drizzle.web3.eth.getAccounts().then(e => drizzle.web3.eth.getBalance(e[0])
+            .then(z => this.setState({ accountBalance: (z/1000000000000000000).toFixed(3)})));
 
         this.setState({ dataKey });
     }
@@ -21,10 +27,12 @@ class Profile extends Component {
 
         const myString = MyStringStore.myString[this.state.dataKey];
 
+
         return (
             <div>
-                <p>This page should show the users address and eth</p>
-                <p>My stored string for example purposes: {myString && myString.value}</p>
+                <p>Your Ethereum Address: {this.state.accountAddress}</p>
+                <p>Balance: {this.state.accountBalance} eth</p>
+                {/*<p>My stored string for example purposes: {myString && myString.value}</p>*/}
             </div>
         );
     }
